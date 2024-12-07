815fd174871e96c5578cf482094627ab43f41ea1
AC-Armitage <chabir2003@gmail.com>
2024-12-07 12:20:34 +0100

    Addedm README.md and url to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="CSTC_PyPack",
+    name="CSTC_PyPack_IP2Bin",
     version="0.1",
     packages=find_packages(),
-    description="An example package for class",
+    description="An example package for class, converts your public ip to binary ",
     author="Abderrahim Chabir",
     author_email="chabirabd@gmail.com",
+    url="https://github.com/AC-Armitage/CSTC_Python"
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: MIT License",
