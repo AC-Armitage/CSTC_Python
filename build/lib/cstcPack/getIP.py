ee5c57a6af87718c44a309299b2f24ace89c6726
Armitage AmphibianOcelot <101667634+AC-Armitage@users.noreply.github.com>
2024-12-14 10:49:35 +0100

    Delete build/lib/cstcPack directory

--- build/lib/cstcPack/getIP.py
+++
@@ -1,9 +0,0 @@
-def getPublicIP():
-    try:
-        response = requests.get('https://api.ipify.org')
-        if response.status_code == 200:
-            return response.text
-        else:
-            return "Failed to get public IP"
-    except Exception as e:
-        return f"Error: {e}"
