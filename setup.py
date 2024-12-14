fa500f0b36d588993b4e8df54b9a3d43355768da
Armitage AmphibianOcelot <101667634+AC-Armitage@users.noreply.github.com>
2024-12-14 10:59:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Abderrahim chabir',
     author_email='chabirabd@gmail.com',
     description='A module for IP address manipulation and subnetting',
-    url='',
+    url='https://github.com/AC-Armitage/CSTC_Python.git',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
